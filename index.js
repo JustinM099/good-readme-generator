94ddff243697201b5b98e829cf1b56fb5f5d2c72
JustinM099 <justinm099@mac.com>
2021-11-08 13:31:13 -0800

    updating functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer')
-const generateMarkdown = require('./generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -36,13 +36,14 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-    inquirer.prompt(questions).then((data) => {
-        fs.writefile('${fileName}', generateMarkdown, (err) =>
+function writeToFile(fileName, data){
+        fs.writeFile(`${fileName}`, data, (err) =>
         err ? console.error('Error! : ' + err) : console.log('Your README has been successfully generated!'))
-    })
+    }
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+       inquirer.prompt(questions).then((data) => writeToFile('yourREADME.md', generateMarkdown(data)))
+    }
 
 // Function call to initialize app
 init();
