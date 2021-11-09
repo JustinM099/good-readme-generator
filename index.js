1f9d90ff493d112434ba0056eb927fcf5f8bc228
JustinM099 <justinm099@mac.com>
2021-11-09 10:40:05 -0800

    formatting

--- index.js
+++ index.js
@@ -3,100 +3,118 @@ const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 const questions = [
-        {type: 'input',
+    {
+        type: 'input',
         message: 'What is the title of your project?',
         name: 'title',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{
+            } else {
                 return true
             }
-        }},
-        {type: 'input',
+        }
+    },
+    {
+        type: 'input',
         message: 'What is a description of your project?',
         name: 'description',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{
+            } else {
                 return true
             }
-        }},
-        {type: 'input',
+        }
+    },
+    {
+        type: 'input',
         message: 'How is your project installed?',
         name: 'installation',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{
+            } else {
                 return true
             }
-        }},
-        {type: 'input',
+        }
+    },
+    {
+        type: 'input',
         message: 'How is your project used?',
         name: 'usage',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{
+            } else {
                 return true
             }
-        }},
-        {type: 'list',
+        }
+    },
+    {
+        type: 'list',
         message: 'Please choose a license type for your open source project',
         choices: ['Apache', 'Boost', 'BSD', 'Creative Commons', 'Eclipse', 'GNU', 'MIT', 'Mozilla', 'None'],
         name: 'license',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{  
+            } else {
                 return true
             }
-        }},
-        {type: 'input',
+        }
+    },
+    {
+        type: 'input',
         message: 'How would a user contribute to your project?',
         name: 'contributing',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{
+            } else {
                 return true
             }
-        }},
-        {type: 'input',
+        }
+    },
+    {
+        type: 'input',
         message: 'What command is required to test your project?', //maybe a default input here
         name: 'tests',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{
+            } else {
                 return true
             }
-        }},
-        {type: 'input',
+        }
+    },
+    {
+        type: 'input',
         message: 'What is your GitHub address?',
-        name: 'questionsGithub'},
-        {type: 'input',
+        name: 'questionsGithub'
+    },
+    {
+        type: 'input',
         message: 'What is your email address?',
         name: 'questionsEmail',
         validate: (response) => {
-            if(!response){
+            if (!response) {
                 return console.log('This response is required.')
-            }else{
+            } else {
                 return true
             }
-        }},
+        }
+    },
 ];
 
-function writeToFile(fileName, data){
-        fs.writeFile(`${fileName}`, data, (err) =>
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}`, data, (err) =>
         err ? console.error('Error! : ' + err) : console.log('Your README has been successfully generated!'))
-    }
+}
 
 function init() {
-       inquirer.prompt(questions).then((data) => writeToFile('yourREADME.md', generateMarkdown(data)))
-    }
+    inquirer.prompt(questions).then((data) => writeToFile('yourREADME.md', generateMarkdown(data)))
+}
 
 init();
 
